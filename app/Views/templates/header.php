fba5f3be0166dcdd8f7f37fb6733bb51a393a03a
HanRos54 <hnfrosidi@gmail.com>
2024-03-22 22:14:17 +0700

    Latihan codeigniter1

---
+++ app/Views/templates/header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+    <title>CodeIgniter Tutorial</title>
+</head>
+<body>
+
+    <h1>Selamat Datang Di Halamanku</h1>
+    <em>Welcome To The Junggle</em>
+</body>
+</html>
